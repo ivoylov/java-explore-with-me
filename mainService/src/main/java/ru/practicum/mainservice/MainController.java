6840dcd720224358d4a8eaa2cc48f0dc31725b70
Ivoylov <vit.Ivoylov@yandex.ru>
2023-10-21 23:02:23 +0500

    feat:create endpoint on main service and get anser from webClient

--- mainService/src/main/java/ru/practicum/mainservice/MainController.java
+++ mainService/src/main/java/ru/practicum/mainservice/MainController.java
@@ -2,8 +2,10 @@ package ru.practicum.mainservice;
 
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 import ru.practicum.mainservice.model.Hit;
@@ -17,19 +19,25 @@ public class MainController {
     @GetMapping("/ping")
     public String ping() {
         log.info("{}; /ping", this.getClass());
-        WebClient webClient = WebClient.builder()
-                .baseUrl("http://localhost:9090/ping")
-                .build();
-        Mono<String> statisticServerAnswer = webClient.get().retrieve().bodyToMono(String.class);
-        return statisticServerAnswer.blockOptional().get();
+        WebClient webClient = WebClient.create();
+        Mono<String> statisticServerAnswer = webClient.get()
+                .uri("http://localhost:9090/ping")
+                .retrieve()
+                .bodyToMono(String.class);
+        return "main server is working\n" + statisticServerAnswer.blockOptional().get();
     }
 
     @PostMapping("/hit")
-    public void putHit(@Validated(Create.class) @RequestBody HitDtoIn hitDtoIn) {
-        WebClient webClient = WebClient.builder()
-                .baseUrl("http://localhost:9090/hit")
-                .build();
-        webClient.post();
+    public Hit putHit(@Validated(Create.class) @RequestBody HitDtoIn hitDtoIn) {
+        log.info("{}; /hit; {}", this.getClass(), hitDtoIn);
+        WebClient webClient = WebClient.create();
+        Mono<Hit> statisticServerAnswer = webClient.post()
+                .uri("http://localhost:9090/hit")
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromValue(hitDtoIn))
+                .retrieve()
+                .bodyToMono(Hit.class);
+        return statisticServerAnswer.blockOptional().get();
     }
 
     @GetMapping("/stats")
