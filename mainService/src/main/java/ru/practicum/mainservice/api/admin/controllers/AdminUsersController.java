d24f297d94f1bc2b2484c16b210fd538cce29037
Ivoylov <vit.Ivoylov@yandex.ru>
2023-12-18 23:03:12 +0500

    feat: refactor AdminUser

--- mainService/src/main/java/ru/practicum/mainservice/api/admin/controllers/AdminUsersController.java
+++ mainService/src/main/java/ru/practicum/mainservice/api/admin/controllers/AdminUsersController.java
@@ -3,6 +3,7 @@ package ru.practicum.mainservice.api.admin.controllers;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import ru.practicum.mainservice.api.admin.services.UsersService;
 import ru.practicum.mainservice.models.user.User;
@@ -11,6 +12,8 @@ import ru.practicum.mainservice.models.user.UserDtoOut;
 import ru.practicum.mainservice.models.user.UserMapper;
 
 import javax.validation.Valid;
+import javax.validation.constraints.Positive;
+import javax.validation.constraints.PositiveOrZero;
 import java.util.List;
 
 @RestController("AdminUsersController")
@@ -22,19 +25,21 @@ public class AdminUsersController {
     UsersService usersService;
 
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public UserDtoOut create(@Valid @RequestBody UserDtoIn userDtoIn) {
         User user = UserMapper.toUser(userDtoIn);
         return UserMapper.toUserDtoOut(usersService.create(user));
     }
 
     @GetMapping
-    public List<User> getAll(@RequestParam(defaultValue = "0") Integer from,
-                             @RequestParam(defaultValue = "10") Integer size,
-                             @RequestParam(defaultValue = "") Long[] ids) {
+    public List<User> getAll(@RequestParam(required = false) List<Long> ids,
+                             @RequestParam(defaultValue = "0") @PositiveOrZero int from,
+                             @RequestParam(defaultValue = "10") @Positive int size) {
         return usersService.getAll(PageRequest.of(from,size), ids);
     }
 
     @DeleteMapping("/{userId}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
     public void delete(@PathVariable Long userId) {
         usersService.delete(userId);
     }
--- mainService/src/main/java/ru/practicum/mainservice/api/admin/services/UsersService.java
+++ mainService/src/main/java/ru/practicum/mainservice/api/admin/services/UsersService.java
@@ -1,9 +1,13 @@
 package ru.practicum.mainservice.api.admin.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import ru.practicum.mainservice.api.admin.repositories.UserRepository;
+import ru.practicum.mainservice.exceptions.ConflictException;
+import ru.practicum.mainservice.exceptions.NotFoundException;
 import ru.practicum.mainservice.models.user.User;
 
 import java.util.Arrays;
@@ -17,24 +21,30 @@ public class UsersService {
     private UserRepository userRepository;
 
     public User create(User user) {
-        return userRepository.save(user);
+        try {
+            return userRepository.save(user);
+        } catch (DataIntegrityViolationException e) {
+            throw new ConflictException("Пользователь уже существует", e.getMessage());
+        }
     }
 
     public User getById(Long userId) {
         return userRepository.findById(userId).orElseThrow();
     }
 
-    public List<User> getAll(Pageable pageable, Long[] ids) {
-        if(ids.length != 0) {
-            List<Long> iterable = Arrays.stream(ids)
-                    .collect(Collectors.toList());
-            return userRepository.findAllById(iterable);
+    public List<User> getAll(Pageable pageable, List<Long> ids) {
+        if(ids.size() != 0) {
+            return userRepository.findAllById(ids);
         } else
             return userRepository.findAll(pageable).getContent();
     }
 
     public void delete(Long userId) {
-        userRepository.deleteById(userId);
+        try {
+            userRepository.deleteById(userId);
+        } catch (EmptyResultDataAccessException e) {
+            throw new NotFoundException(String.format("пользователь с id=%s не найден", userId));
+        }
     }
 
 }
