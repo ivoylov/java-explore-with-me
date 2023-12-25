8905d44c046a9a95a8ed32bcab86a30cf5dc1fdd
Ivoylov <vit.Ivoylov@yandex.ru>
2023-12-25 14:59:34 +0500

    style:clean

--- main-service/src/main/java/ru/practicum/mainservice/services/StatsService.java
+++ main-service/src/main/java/ru/practicum/mainservice/services/StatsService.java
@@ -17,31 +17,27 @@ public class StatsService {
 
     private static final long INTERVAL_HOUR = 1;
     private final StatsClient client;
-    
+
     public void hit(EndpointHitDto endpointHitDto) {
         client.hit(endpointHitDto);
     }
 
     public List<ViewStatsDto> getStatsSearchInterval(LocalDateTime start, boolean unique, List<String> uris) {
-
         LocalDateTime endS = LocalDateTime.now().plusHours(INTERVAL_HOUR);
-
         return getStatsSearchInterval(start, endS, unique, uris);
     }
 
     public List<ViewStatsDto> getStatsSearchInterval(LocalDateTime start, LocalDateTime end, boolean unique, List<String> uris) {
-
         Object statsList = client.stats(start.minusHours(INTERVAL_HOUR), end, unique, uris).getBody();
         ObjectMapper mapper = new ObjectMapper();
-        return mapper.convertValue(statsList, new TypeReference<List<ViewStatsDto>>() {
+        return mapper.convertValue(statsList, new TypeReference<>() {
         });
     }
 
     public List<ViewStatsDto> getStatsSearch(LocalDateTime start, LocalDateTime end, boolean unique, List<String> uris) {
-
         Object statsList = client.stats(start, end, unique, uris).getBody();
         ObjectMapper mapper = new ObjectMapper();
-        return mapper.convertValue(statsList, new TypeReference<List<ViewStatsDto>>() {
+        return mapper.convertValue(statsList, new TypeReference<>() {
         });
     }
 
